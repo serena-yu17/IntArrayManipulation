eefd4014b273b79e7563f7adc61f5e766b971a04
Serena <serena.f.yu@gmail.com>
2018-10-29 20:25:49 +1100

    added reference

--- ArrayManipTester/ArrayManipTester.cs
+++ ArrayManipTester/ArrayManipTester.cs
@@ -1,6 +1,5 @@
 using ArrayManipulation.Operations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace ArrayManipTester
 {
@@ -11,7 +10,7 @@ namespace ArrayManipTester
         public void reverseTestMethod1()
         {
             int[] testArr = null;
-            IntegerManipulationService.reverse(testArr);
+            ArrayManipService.reverse(testArr);
             CollectionAssert.AreEqual(testArr, null);
         }
 
@@ -19,7 +18,7 @@ namespace ArrayManipTester
         public void reverseTestMethod2()
         {
             int[] testArr = new int[] { 0 };
-            IntegerManipulationService.reverse(testArr);
+            ArrayManipService.reverse(testArr);
             CollectionAssert.AreEqual(testArr, new int[] { 0 });
         }
 
@@ -27,7 +26,7 @@ namespace ArrayManipTester
         public void reverseTestMethod3()
         {
             int[] testArr = new int[] { 0, 1, 2, 3, 4 };
-            IntegerManipulationService.reverse(testArr);
+            ArrayManipService.reverse(testArr);
             CollectionAssert.AreEqual(testArr, new int[] { 4, 3, 2, 1, 0 });
         }
 
@@ -35,7 +34,7 @@ namespace ArrayManipTester
         public void reverseTestMethod4()
         {
             int[] testArr = new int[] { 0, 1, 2, 3 };
-            IntegerManipulationService.reverse(testArr);
+            ArrayManipService.reverse(testArr);
             CollectionAssert.AreEqual(testArr, new int[] { 3, 2, 1, 0 });
         }
 
@@ -43,7 +42,7 @@ namespace ArrayManipTester
         public void reverseTestMethod5()
         {
             int[] testArr = new int[] { 4, 3, 2, 1, 0 };
-            IntegerManipulationService.reverse(testArr);
+            ArrayManipService.reverse(testArr);
             CollectionAssert.AreEqual(testArr, new int[] { 0, 1, 2, 3, 4 });
         }
 
@@ -51,7 +50,7 @@ namespace ArrayManipTester
         public void reverseTestMethod6()
         {
             int[] testArr = new int[] { 4, 3, 2, 1 };
-            IntegerManipulationService.reverse(testArr);
+            ArrayManipService.reverse(testArr);
             CollectionAssert.AreEqual(testArr, new int[] { 1, 2, 3, 4 });
         }
 
@@ -59,7 +58,7 @@ namespace ArrayManipTester
         public void reverseTestMethod7()
         {
             int[] testArr = new int[] { 4, 2, 3, 1, 5 };
-            IntegerManipulationService.reverse(testArr);
+            ArrayManipService.reverse(testArr);
             CollectionAssert.AreEqual(testArr, new int[] { 5, 1, 3, 2, 4 });
         }
 
@@ -67,7 +66,7 @@ namespace ArrayManipTester
         public void delTestMethod8()
         {
             int[] testArr = null;
-            IntegerManipulationService.deletePart(testArr, 1);
+            ArrayManipService.deletePart(testArr, 1);
             CollectionAssert.AreEqual(testArr, null);
         }
 
@@ -75,7 +74,7 @@ namespace ArrayManipTester
         public void delTestMethod9()
         {
             int[] testArr = new int[] { 0 };
-            IntegerManipulationService.deletePart(testArr, 1);
+            ArrayManipService.deletePart(testArr, 1);
             CollectionAssert.AreEqual(testArr, new int[] { 0 });
         }
 
@@ -83,7 +82,7 @@ namespace ArrayManipTester
         public void delTestMethod10()
         {
             int[] testArr = new int[] { 0 };
-            var result = IntegerManipulationService.deletePart(testArr, 0);
+            var result = ArrayManipService.deletePart(testArr, 0);
             CollectionAssert.AreEqual(result, new  int[0]);
         }
 
@@ -91,7 +90,7 @@ namespace ArrayManipTester
         public void delTestMethod11()
         {
             int[] testArr = new int[] { 0, 1, 4, 7, 9, 100 };
-            var result = IntegerManipulationService.deletePart(testArr, 7);
+            var result = ArrayManipService.deletePart(testArr, 7);
             CollectionAssert.AreEqual(result, testArr);
         }
 
@@ -99,7 +98,7 @@ namespace ArrayManipTester
         public void delTestMethod12()
         {
             int[] testArr = new int[] { 0, 1, 4, 7, 9, 100 };
-            var result = IntegerManipulationService.deletePart(testArr, 5);
+            var result = ArrayManipService.deletePart(testArr, 5);
             CollectionAssert.AreEqual(result, new int[] { 0, 1, 4, 7, 9 });
         }
 
@@ -107,7 +106,7 @@ namespace ArrayManipTester
         public void delTestMethod13()
         {
             int[] testArr = new int[] { 0, 1, 4, 7, 9, 100 };
-            var result = IntegerManipulationService.deletePart(testArr, 0);
+            var result = ArrayManipService.deletePart(testArr, 0);
             CollectionAssert.AreEqual(result, new int[] { 1, 4, 7, 9, 100 });
         }
 
@@ -115,7 +114,7 @@ namespace ArrayManipTester
         public void delTestMethod14()
         {
             int[] testArr = new int[] { 0, 1, 4, 7, 9, 100 };
-            var result = IntegerManipulationService.deletePart(testArr, 3);
+            var result = ArrayManipService.deletePart(testArr, 3);
             CollectionAssert.AreEqual(result, new int[] { 0, 1, 4, 9, 100 });
         }
     }
